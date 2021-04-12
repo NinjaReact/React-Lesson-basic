ebdd55a404c4150cd2f3718796dd018fc935ad58
ReactNinja <jsninjacode@gmail.com>
2021-04-12 12:03:12 +0300

    jsx first task

---
+++ React-mega-basic/basic/jsx-basic/src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const Books = props =>{
+  return (
+    <div>
+      <h2>{props.name ? props.name : 'default book'}</h2>
+      <p>{props.year}</p>
+      <p>{props.price}</p>
+    </div>
+  )
+}
+
+const Apps = () =>{
+  return (
+    <div>
+      <Books name='Vue' year='2010' price='1111'/>
+      <Books name='Angular' year='2015' price='2222'/>
+      <Books name='React' year='2020' price='3333'/>
+    </div>
+  )
+}
+ReactDOM.render(React.createElement(Apps) , document.getElementById('root'))
+
+reportWebVitals();
